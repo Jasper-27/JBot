6ffdf347568a15fef231d8f2aaed9732aec874bb
Jasper Cox <57488312+Jasper-27@users.noreply.github.com>
2020-09-23 18:49:33 +0100

    Basic spam filter added

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ const client = new Discord.Client()
 const channel = client.channels.cache.get('644539373077004299')
 const sortArray = require('sort-array')
 
-
+//Allows exectuing programs on the server
 const { exec } = require("child_process");
 
 // 5 mins, 60 seconds in a min, 1000 ms in a second
 const saveTime = 5 * 60 * 1000; /* ms */
 
-const RNCCap = 6
+const SpamCap = 6
 
 
 //https://discord.com/oauth2/authorize?client_id=717442260131774485&scope=bot
@@ -44,7 +44,7 @@ class user {
   RNC
 }
 
-
+var keywords = ["nice", "fortune", "hello there", "ping", "marco", "marco!", "list scores", "fuck you <@!717442260131774485>", "neofetch"]
 
 
 //Reads the userfile, or creates it if it doesn't exist
@@ -73,76 +73,7 @@ function saveToFiles(){
   fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
 }
 
-function niceCode(msg){
-  try{
-      theUserIndex = findUsers(msg.member.id)
-  }catch{
-    return
-  }
 
-
-  var now = new Date();
-  now = now - 0
-
-
-  if (theUserIndex == null){
-
-    //Creates a new user with basic entry in the json file
-    console.log("New user: " + msg.member.id)
-    newUser = {ID:msg.member.id, UName:msg.author.username, NC: 1, RNC: 0,}
-    users.push(newUser)
-    console.log(newUser)
-    msg.channel.send("This is " + msg.member.user.username + "'s first \"Nice\" ");
-
-  }else{
-
-    users[theUserIndex].NC++
-    msg.channel.send(msg.member.user.username + "'s \"Nice\" count has gone up to " + users[theUserIndex].NC);
-
-    users[theUserIndex].RNC++
-    console.log(msg.member.user.username + "'s recent \"Nice\" count has gone up to " + users[theUserIndex].RNC)
-
-    if (users[theUserIndex].RNC > 3){
-      msg.reply("Watch it sunshine")
-
-    }
-
-    // The kicking can cause an error. Not an issue now but soon will cause the app to crash
-    // I can't find a work around at the moment
-
-
-    // The try is not working. Not sure why
-    if (users[theUserIndex].RNC > RNCCap){
-      try {
-        users[theUserIndex].NC = 0;
-        msg.channel.send("Kicking user: " + msg.author.username + ", for spamming");
-        msg.author.send("Don't spam. It is just annoying and gets you kicked");
-        msg.member.kick();
-      }catch(err){
-        msg.channel.send("Could not kick user. You got lucky this time")
-        console.log("Could not kick user")
-        console.log(err)
-      }
-
-    }
-
-  }
-
-
-  if (lastTime == null){
-    lastTime = now
-    console.log("Time has been set")
-  }
-
-
-  if ((now - lastTime) > saveTime){
-    lastTime = now;
-    console.log("Saving users");
-
-    saveToFiles()
-  }
-
-}
 
 function fortuneCode(msg){
   exec("fortune", (error, stdout, stderr) => {
@@ -157,7 +88,24 @@ function fortuneCode(msg){
         return;
     }
     msg.reply(`${stdout}`);
-});
+  });
+
+}
+
+function neofetchCode(msg){
+  exec("neofetch --stdout", (error, stdout, stderr) => {
+    if (error) {
+        //console.log(`error: ${error.message}`);
+        msg.reply("Could not run neofetch")
+        return;
+    }
+    if (stderr) {
+        msg.reply(`There was an error: ${stderr}`)
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    msg.reply(`${stdout}`);
+  });
 
 }
 
@@ -196,7 +144,7 @@ client.on('ready', () => {
 
 
 
-  // Does this but work? Well at some point it did 
+  // Does this  work? Well at some point it did 
 
 
   //Sets the bots username and activity
@@ -227,37 +175,117 @@ client.on('guildMemberAdd', member => {
 //Runs when the message is read
 client.on('message', msg => {
 
+
+
+
+  //Checks if the message is one of the keywords
+  var found = false
+  for ( var i = 0; i < keywords.length && !found; i++) {
+    if (keywords[i] === msg.content.toLowerCase()) {
+      found = true;
+      console.log(msg.content)
+      break;
+    }
+  }
+
+
+  //If the message is one of the keywords
+  if (found === true){
+
+
+    try{
+      theUserIndex = findUsers(msg.member.id)
+    }catch{
+      
+    }
+
+    if (theUserIndex == null){
+
+      //Creates a new user with basic entry in the json file
+      console.log("New user: " + msg.member.id)
+      newUser = {ID:msg.member.id, UName:msg.author.username, NC: 0, RNC: 0,}
+      users.push(newUser)
+      console.log(newUser)
+      msg.channel.send("This is " + msg.member.user.username + "'s first use of the jBot ");
+  
+    }else{
+      users[theUserIndex].RNC++
+      console.log(msg.member.user.username + "'s spam count has gone up to " + users[theUserIndex].RNC)
+      
+
+      if (users[theUserIndex].RNC > 3){
+        msg.reply("Watch it sunshine")
+  
+      }
+  
+      // The kicking can cause an error. Not an issue now but soon will cause the app to crash
+      // I can't find a work around at the moment
+  
+      // The try is not working. Not sure why
+      if (users[theUserIndex].RNC > SpamCap){
+        try {
+          users[theUserIndex].NC = 0;
+          msg.channel.send("Kicking user: " + msg.author.username + ", for spamming");
+          msg.author.send("Don't spam. It is just annoying and gets you kicked");
+          msg.member.kick();
+        }catch(err){
+          msg.channel.send("Could not kick user. You got lucky this time")
+          console.log("Could not kick user")
+          console.log(err)
+        }
+  
+      }
+
+      //The message replies 
+      if (msg.content === 'ping') {
+        msg.reply('Pong!');
+      }
+
+      if (msg.content.toLowerCase() === 'marco' || msg.content.toLowerCase() === 'marco!' ){
+        msg.reply("Polo!")
+      }
+
+      if (msg.content.toLowerCase() === 'hello there') {
+        msg.channel.send('General Kenobi! You are a bold one');
+      }
+
+
+      if (msg.content.toLowerCase() == "list scores"){
+        rankings(msg)
+      }
+
+      if (msg.content.toLowerCase() == 'fortune'){
+        fortuneCode(msg)
+      }
+
+      if (msg.content.toLowerCase() == 'neofetch'){
+        neofetchCode(msg)
+      }
+
+      if (msg.content.toLowerCase() == "fuck you <@!717442260131774485>"){
+      msg.channel.send("Fuck you " + msg.author.username )
+      }
+
+      //The Nice count code
+      if (msg.content.toLowerCase() === 'nice'){
+        msg.channel.send(msg.member.user.username + "'s \"Nice\" count has gone up to " + users[theUserIndex].NC)
+        users[theUserIndex].NC++
+        //niceCode(msg)
+      }
+
+      var now = new Date();
+      now = now - 0 // makes now a number. Don't ask why
+
+    }
+  } 
+
+
+
+
   console.log(msg.author.username + " : " + msg.content)
 
-  if(msg.author === client.id){
-    console.log("message from me")
-    return
-  }
 
 
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-
-  if (msg.content.toLowerCase() === 'marco' || msg.content.toLowerCase() === 'marco!' ){
-    msg.reply("Polo!")
-  }
-
-  if (msg.content.toLowerCase() === 'hello there') {
-    msg.channel.send('General Kenobi! You are a bold one');
-  }
-
-
-  if (msg.content.toLowerCase() == "list scores"){
-    rankings(msg)
-
-  }
-
-  if (msg.content.toLowerCase() === 'fortune'){
-
-    fortuneCode(msg)
-
-  }
 
 /*
  this bit is broken. Takes ages and too large a group will fuck it.
@@ -285,15 +313,7 @@ Write a routine that finds each users position individually. Much better and mor
 
 */
 
-  var now = new Date();
-  now = now - 0 // makes now a number. Don't ask why
 
-  //The Nice count code
-  if (msg.content.toLowerCase() === 'nice'){
-
-    niceCode(msg)
-
-  }
 
 
 
@@ -302,6 +322,10 @@ Write a routine that finds each users position individually. Much better and mor
     msg.react('🤮')
     msg.author.send('Go uwu somewhere else');
   }
+
+
+
+  
 
   // Commands (Soon this will be a different thing )
 
@@ -326,9 +350,10 @@ Write a routine that finds each users position individually. Much better and mor
         }catch{
           msg.channel.send("Could not kick user")
         }
-
-
       }
+
+      
+
     }
   }
 })
@@ -357,8 +382,9 @@ function clearRN(){
 
 }
 
-
-setInterval(clearRN, (5 * 60 * 1000));   //Runs the clear nice function every 5 mins
+// hour * min * sec * ms
+setInterval(clearRN, (60 * 60 * 1000));   //Runs the clear nice function every time interval 
+setInterval(saveToFiles, (60 * 60 * 1000));   //save the users every time interval 
 
 
 //Says when it disconnects
